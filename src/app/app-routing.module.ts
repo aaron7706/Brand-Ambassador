bcfcc80df1e7219889cf2cbe29ea50af8b49aa79
Shwetayadav123 <yadavshweta1202@gmail.com>
2025-04-10 19:58:18 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BradingPageOneComponent } from './brading-page-one/brading-page-one.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
