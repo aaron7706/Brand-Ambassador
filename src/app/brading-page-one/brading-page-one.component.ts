54d2f4b43e684c767dba2bd1cad9e2ec601b32b0
Diksha Pawar <93197187+dikshapawar452@users.noreply.github.com>
2025-04-15 15:27:42 +0530

    at scroll video should auto play

--- src/app/brading-page-one/brading-page-one.component.ts
+++ src/app/brading-page-one/brading-page-one.component.ts
@@ -7,6 +7,29 @@ import { ServicesService } from '../services.service';
   styleUrls: ['./brading-page-one.component.scss']
 })
 export class BradingPageOneComponent {
+  videoElement: HTMLVideoElement | undefined;
+  onVideoLoad(video: HTMLVideoElement): void {
+    this.videoElement = video;
+    this.setupIntersectionObserver();
+  }
 
+  setupIntersectionObserver(): void {
+    const options = {
+      root: null, // use the viewport as the root
+      rootMargin: '0px',
+      threshold: 0.5 // trigger when 50% of the video is visible
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.videoElement?.play();
+        } else {
+          this.videoElement?.pause();
+        }
+      });
+    }, options);
+    observer.observe(this.videoElement as Element);
+  }
   
 }
--- src/app/nadia-afrin-min/nadia-afrin-min.component.ts
+++ src/app/nadia-afrin-min/nadia-afrin-min.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./nadia-afrin-min.component.scss']
 })
 export class NadiaAfrinMinComponent {
+  videoElement: HTMLVideoElement | undefined;
+  onVideoLoad(video: HTMLVideoElement): void {
+    this.videoElement = video;
+    this.setupIntersectionObserver();
+  }
 
+  setupIntersectionObserver(): void {
+    const options = {
+      root: null, // use the viewport as the root
+      rootMargin: '0px',
+      threshold: 0.5 // trigger when 50% of the video is visible
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.videoElement?.play();
+        } else {
+          this.videoElement?.pause();
+        }
+      });
+    }, options);
+    observer.observe(this.videoElement as Element);
+  }
+  
 }
